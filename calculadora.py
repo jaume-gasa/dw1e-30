f869a30ee1b6dadc22f1fa3ed942fbffa18006fa
Jaume Gasa <ua.jaume@gmail.com>
2026-01-30 17:34:47 +0100

    Añadido código base

---
+++ calculadora.py
@@ -0,0 +1,48 @@
+class OperacionesCalculadora:
+    def __init__(self):
+        self.resultado_actual = 0
+
+    # --- Operaciones Básicas ---
+    def sumar(self, a, b):
+        pass
+
+    def restar(self, a, b):
+        pass
+
+    def multiplicar(self, a, b):
+        pass
+
+    def dividir(self, a, b):
+        pass
+
+    # --- Operaciones Avanzadas ---
+    def potencia(self, base, exponente):
+        pass
+
+    def raiz_cuadrada(self, n):
+        pass
+
+    def porcentaje(self, total, porcentaje):
+        pass
+
+    def modulo(self, a, b):
+        pass
+
+    # --- Funciones Especiales ---
+    def limpiar_pantalla(self):
+        pass
+
+    def deshacer_ultimo(self):
+        pass
+
+    def logaritmo(self, n):
+        pass
+
+    def seno(self, angulo):
+        pass
+
+    def coseno(self, angulo):
+        pass
+
+    def tangente(self, angulo):
+        pass
